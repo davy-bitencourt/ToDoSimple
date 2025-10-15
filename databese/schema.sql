e39174dc429d834553810b179e6f82164f3008c3
Emerson Okemoto <okemoto100@gmail.com>
2025-10-15 20:22:11 -0300

    Implementaçao de controler

--- databese/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS tasks (
-    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    title TEXT NOT NULL,
-    notes TEXT,
-    is_done INTEGER DEFAULT 0, -- Status: 0 = pendente, 1 = concluída
-    created_at TEXT NOT NULL DEFAULT (datetime('now')),
-    updated_at TEXT NOT NULL DEFAULT (datetime('now')),
-);
